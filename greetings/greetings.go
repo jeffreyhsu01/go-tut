49663e85cabdc53d4447f25e0d5ae766cbeebdf9
Jeffrey Hsu <jeffreyhsu01@gmail.com>
2022-03-04 14:43:20 -0500

    introduced maps and backward compatibility

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -19,6 +19,23 @@ func Hello(name string) (string, error) { // Function name, Parameter type, Retu
 	return message, nil
 }
 
+// Hellos returns a map that associatates each of the named people
+// with a greeting message.
+func Hellos(names []string) (map[string]string, error) {
+	//  A map to associate names with  messages
+	messages := make(map[string]string)
+	// Loop through the received slice of names, calling
+	// the Hello function to get a message for each name.
+	for _, name := range names {
+		message, err := Hello(name)
+		if err != nil {
+			return nil, err
+		}
+		messages[name] = message
+	}
+	return messages, nil
+}
+
 // init seeds the rand package with the current time
 func init() {
 	rand.Seed(time.Now().UnixNano())
